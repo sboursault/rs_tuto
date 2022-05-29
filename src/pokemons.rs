3448cd137b228afd5b56a4450b4a7ec987573492
sboursault <sboursault@protonmail.com>
2022-05-29 07:36:31 +0200

    derive Debug

--- src/pokemons.rs
+++ src/pokemons.rs
@@ -1,6 +1,9 @@
 use rand::Rng;
 
-//#[derive(Copy)]  // see also #[derive(Debug, Copy, Clone)]
+#[derive(Debug)]  // see also #[derive(Debug, Copy, Clone)]
+// Pokemons can't derive Copy because it contains a String (String, like Vec and any other variable-sized container, contains a pointer to some variable amount of heap memory)
+// The only correct way to copy a String is to allocate a new block of heap memory to copy all the characters into, which is what String's Clone implementation does.
+// someone recommends to read this to be productive with Rust : https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html
 pub struct Pokemon {
     pub name: String,
     pub hp: i32,
@@ -39,11 +42,23 @@ impl Fighter for Pokemon {
     }
 }
 
+fn say_hello(p: &Pokemon) {
+    println!("hello {}", p.name);
+}
+
+fn evolve(p: &mut Pokemon) {
+    if p.name == "Taupiqueur" {
+        p.name = "Triopikeur".to_string();
+    }
+}
+
 
 #[cfg(test)] // Only compiles when running tests
 mod tests {
     use super::Pokemon;
     use super::Fighter;
+    use super::say_hello;
+    use super::evolve;
 
     #[test]
     fn verify_greet() {
@@ -58,10 +73,25 @@ mod tests {
         assert!(strength <= 100);
     }
 
-    /*#[test]
-    fn verify_trait_copy() {
+    #[test]
+    fn verify_trait_debug() {
         let taupi = Pokemon::new("Taupiqueur", 10);
-        let taupi_2 = taupi;
-        assert_eq!(taupi.hp, 10);
-    }*/
+        let formated = format!("{:?}", taupi);  // breaks without the Debug trait
+        assert!(formated.contains("Pokemon { name: \"Taupiqueur\", "));
+    }
+
+    #[test]
+    fn verify_say_hello_ref() {
+        let taupi = Pokemon::new("Taupiqueur", 10);
+        say_hello(&taupi);  // `taupi` is borrowed for the time of the call
+        say_hello(&taupi);  // works because we just pass a reference to say_hello
+    }
+
+    #[test]
+    fn verify_evolve_mut_ref() {
+        let mut taupi = Pokemon::new("Taupiqueur", 10);
+        evolve(&mut taupi);  // `taupi` is borrowed mutably, works because `taupi` is declared mutable
+        say_hello(&taupi);  // works because we just pass a reference to say_hello
+        assert_eq!(taupi.name, "Triopikeur");
+    }
 }
